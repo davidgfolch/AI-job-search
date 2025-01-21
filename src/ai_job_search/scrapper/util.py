20e547ea3c5160f07b81eea64f271b8561a20983
davidgfolch@gmail.com <davidgfolch@gmail.com>
2025-01-21 13:00:30 +0100

    Scrapper: .env default JOBS_SEARCH 4 all sites, improved error description.

--- src/ai_job_search/scrapper/util.py
+++ src/ai_job_search/scrapper/util.py
@@ -9,9 +9,14 @@ def getAndCheckEnvVars(site: str):
     mail = os.environ.get(f'{site}_EMAIL')
     pwd = os.environ.get(f'{site}_PWD')
     search = os.environ.get(f'{site}_JOBS_SEARCH')
+    if not search:
+        search = os.environ.get('JOBS_SEARCH')
     if not mail or not pwd or not search:
-        print(yellow('Please read README.md first'))
-        print(yellow('Set up .venv file with email pwd & search params.'))
+        print(yellow('Set up .venv file with the following keys:'))
+        print(yellow(f'{site}_EMAIL' if not mail else '',
+                     f'{site}_PWD' if not pwd else '',
+                     f'{site}_JOBS_SEARCH' if not search else ''))
+        print(yellow('Please read README.md for more info'))
         exit()
     return mail, pwd, search
 
