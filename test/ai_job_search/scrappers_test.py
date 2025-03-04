cd3aae4bc6cc05458a5cd1be5165ddc08278fbce
davidgfolch@gmail.com <davidgfolch@gmail.com>
2025-03-04 13:51:03 +0100

    Testing: scrappers

--- test/ai_job_search/scrappers_test.py
+++ test/ai_job_search/scrappers_test.py
@@ -1,7 +1,7 @@
 import pytest
 from unittest.mock import patch, MagicMock
 
-from ai_job_search.scrappers import (
+from ai_job_search.scrapper_main import (
     SCRAPPERS, runAllScrappers, runSpecifiedScrappers)
 
 
@@ -19,14 +19,14 @@ def mock_scrappers():
 
 @pytest.fixture
 def mock_selenium():
-    with patch('ai_job_search.scrappers.SeleniumUtil', MagicMock):
+    with patch('ai_job_search.scrapper_main.SeleniumUtil', MagicMock):
         yield
 
 
 def test_runAllScrappers(mock_scrappers):
-    with patch('ai_job_search.scrappers.consoleTimer', return_value=None), \
-            patch('ai_job_search.scrappers.getDatetimeNow', return_value=0), \
-            patch('ai_job_search.scrappers.getSeconds', return_value=0):
+    with patch('ai_job_search.scrapper_main.consoleTimer', return_value=None), \
+            patch('ai_job_search.scrapper_main.getDatetimeNow', return_value=0), \
+            patch('ai_job_search.scrapper_main.getSeconds', return_value=0):
         runAllScrappers(waitBeforeFirstRuns=False,
                         starting=False, startingAt=None, noLoop=True)
         for scrapper in SCRAPPERS.values():
@@ -35,9 +35,9 @@ def test_runAllScrappers(mock_scrappers):
 
 
 def test_runAllScrappers_with_wait(mock_scrappers):
-    with patch('ai_job_search.scrappers.consoleTimer', return_value=None), \
-            patch('ai_job_search.scrappers.getDatetimeNow', return_value=0), \
-            patch('ai_job_search.scrappers.getSeconds', return_value=0):
+    with patch('ai_job_search.scrapper_main.consoleTimer', return_value=None), \
+            patch('ai_job_search.scrapper_main.getDatetimeNow', return_value=0), \
+            patch('ai_job_search.scrapper_main.getSeconds', return_value=0):
         runAllScrappers(waitBeforeFirstRuns=True,
                         starting=False, startingAt=None, noLoop=True)
         for scrapper in SCRAPPERS.values():
@@ -52,10 +52,10 @@ def test_runSpecifiedScrappers(mock_scrappers):
 
 
 def test_runSpecifiedScrappers_invalid_name(mock_scrappers):
-    with patch('sys.argv', ['scrappers.py', 'InvalidScrapper']), \
-            patch('ai_job_search.scrappers.red',
+    with patch('sys.argv', ['scrapper_main.py', 'InvalidScrapper']), \
+            patch('ai_job_search.scrapper_main.red',
                   return_value='Invalid scrapper web page name InvalidScrapper'), \
-            patch('ai_job_search.scrappers.yellow',
+            patch('ai_job_search.scrapper_main.yellow',
                   return_value='Available web page scrapper names: dict_keys([\'Infojobs\', \'Linkedin\', \'Glassdoor\', \'Tecnoempleo\', \'Indeed\'])'):
         runSpecifiedScrappers(['InvalidScrapper'])
         for scrapper in SCRAPPERS.values():
