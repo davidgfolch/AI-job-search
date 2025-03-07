ef7f72d13272fac4fd2d4fc5703e493f4a012bba
davidgfolch@gmail.com <davidgfolch@gmail.com>
2025-03-07 17:13:33 +0100

    Automation: run terminal scripts in terminator tabs

--- run.sh
+++ run.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# Abrir cada script en una nueva pestaña del terminal usando terminator
-terminator --new-tab -e "bash -c './run_1_Mysql.sh; exec bash'" --title="run_1_Mysql" && \
+dependant_scripts() {
     terminator --new-tab -e "bash -c './run_2_Scraper.sh; exec bash'" --title="run_2_Scraper"
-terminator --new-tab -e "bash -c './run_3_AiEnricher.sh; exec bash'" --title="run_3_AiEnricher"
-terminator --new-tab -e "bash -c './run_4_Viewer.sh; exec bash'" --title="run_4_Viewer"
+    terminator --new-tab -e "bash -c './run_3_AiEnricher.sh; exec bash'" --title="run_3_AiEnricher"
+    terminator --new-tab -e "bash -c './run_4_Viewer.sh; exec bash'" --title="run_4_Viewer"
+}
+
+terminator --new-tab -e "bash -c './run_1_Mysql.sh; exec bash'" --title="run_1_Mysql" && \
+    dependant_scripts
