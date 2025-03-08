e9c676fe7519e0164442cbbe05a261e055b65488
davidgfolch@gmail.com <davidgfolch@gmail.com>
2025-03-08 11:50:23 +0100

    run.sh main entry point. Bash scripts into specific folder

--- run.sh
+++ run.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-dependant_scripts() {
-    terminator --new-tab -e "bash -c './run_2_Scraper.sh; exec bash'" --title="run_2_Scraper"
-    terminator --new-tab -e "bash -c './run_3_AiEnricher.sh; exec bash'" --title="run_3_AiEnricher"
-    terminator --new-tab -e "bash -c './run_4_Viewer.sh; exec bash'" --title="run_4_Viewer"
+wait_for_mysql() {
+    while ! nc -z 127.0.0.1 3306; do
+        echo "Waiting for MySQL server to be ready..."
+        sleep 2
+    done
 }
 
-terminator --new-tab -e "bash -c './run_1_Mysql.sh; exec bash'" --title="run_1_Mysql" && \
+dependant_scripts() {
+    terminator --new-tab -e "bash -c './scripts/run_2_Scraper.sh; exec bash'" --title="run_2_Scraper"
+    terminator --new-tab -e "bash -c './scripts/run_3_AiEnricher.sh; exec bash'" --title="run_3_AiEnricher"
+    terminator --new-tab -e "bash -c './scripts/run_4_Viewer.sh; exec bash'" --title="run_4_Viewer"
+    terminator --new-tab -e "bash -c ' clear && sudo journalctl  -u ollama -f | perl ~/scripts/colorTail.pl \"gpu|cuda\"; exec bash'" --title="run_4_Viewer"
+}
+
+terminator --new-tab -e "bash -c './scripts/run_1_Mysql.sh; exec bash'" --title="run_1_Mysql" && \
+    wait_for_mysql && \
     dependant_scripts
