8e38714934c5aefe6a53761db2358b4aa252c1dd
davidgfolch@gmail.com <davidgfolch@gmail.com>
2025-10-14 11:57:45 +0200

    streamlit disable file watch

--- scripts/run_4_Viewer.sh
+++ scripts/run_4_Viewer.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 .venv/bin/python3 -V
-.venv/bin/python3 -m streamlit run src/ai_job_search/view.py
+
+if [[ "$*" == *"DEV"* ]]
+then
+    echo "DEV Mode using streamlit fileWatcher for changes"
+    echo "If you get an error on maxFileWatch use: "
+    echo "cat /proc/sys/fs/inotify/max_user_watches"
+    echo "sudo sysctl fs.inotify.max_user_watches=524288"
+    .venv/bin/python3 -m streamlit run src/ai_job_search/view.py --server.headless true
+else
+    echo "DEV Mode disabled by default, activate it using: ./run_4_Viewer.sh dev"
+    .venv/bin/python3 -m streamlit run src/ai_job_search/view.py --server.fileWatcherType none --server.headless true
+fi
+
