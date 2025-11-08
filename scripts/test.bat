ce7f9dac74ce96ec9c2439461c146ce3f2a2d19f
David G. Folch <davidgfolch@gmail.com>
2025-11-08 16:31:45 +0100

    test.bat

--- scripts/test.bat
+++ scripts/test.bat
@@ -19,13 +19,19 @@ for /d %%a in (apps\*) do (
         if exist "%~dp0" (
             rem prefer calling commands directly; errors will be shown by those tools
         )
-        uv sync
         uv run pytest
-        popd
+        uv run coverage run -m pytest
+        uv run coverage report -m
+        uv run coverage xml
+        uv run coverage-badge -o coverage-badge.svg -f        popd
     ) else (
         echo Testing %%~nxa library...
         pushd "%%~fa"
         poetry run pytest
+        poetry run coverage run -m pytest
+        poetry run coverage report -m
+        poetry run coverage xml
+        poetry run coverage-badge -o coverage-badge.svg -f
         popd
     )
 )
