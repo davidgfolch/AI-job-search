95666ab514135fe9c8e638f3965410d76d6149fd
David G. Folch <davidgfolch@gmail.com>
2025-12-16 12:50:35 +0100

    commonlib: move test code to test scope

--- packages/commonlib/test/power_test.py
+++ packages/commonlib/test/power_test.py
@@ -1,28 +1,7 @@
 import unittest
-import sys
-import os
 import time
-import importlib.util
-
-# Helper to import module from path
-def import_from_path(module_name, file_path):
-    spec = importlib.util.spec_from_file_location(module_name, file_path)
-    module = importlib.util.module_from_spec(spec)
-    sys.modules[module_name] = module
-    spec.loader.exec_module(module)
-    return module
-
-# Define paths
-base_dir = os.path.dirname(__file__)
-keep_system_awake_path = os.path.join(base_dir, 'keep_system_awake.py')
-wake_timer_path = os.path.join(base_dir, 'wake_timer.py')
-
-# Import modules directly
-keep_system_awake_mod = import_from_path('keep_system_awake_mod', keep_system_awake_path)
-wake_timer_mod = import_from_path('wake_timer_mod', wake_timer_path)
-
-KeepSystemAwake = keep_system_awake_mod.KeepSystemAwake
-WakeableTimer = wake_timer_mod.WakeableTimer
+from commonlib.keep_system_awake import KeepSystemAwake
+from commonlib.wake_timer import WakeableTimer
 
 class TestPowerManagement(unittest.TestCase):
     def test_keep_system_awake(self):
