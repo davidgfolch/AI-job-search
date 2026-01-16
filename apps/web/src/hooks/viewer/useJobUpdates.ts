7e4515601134f5ef6dd1f02918d52a1df0c789d8
David G. Folch <davidgfolch@gmail.com>
2026-01-16 17:09:52 +0100

    web: fix api trigger to check new jobs

--- apps/web/src/hooks/viewer/useJobUpdates.ts
+++ apps/web/src/hooks/viewer/useJobUpdates.ts
@@ -2,8 +2,12 @@ import { useQuery } from '@tanstack/react-query';
 import { jobsApi, type JobListParams } from '../../api/jobs';
 
 export const useJobUpdates = (filters: JobListParams, knownJobIds: Set<number>) => {
+    // Exclude page from queryKey so that pagination doesn't reset the polling timer
+    // We want the check to run every 5 minutes regardless of which page the user is on
+    const { page, ...filtersWithoutPage } = filters;
+    
     const { data } = useQuery({
-        queryKey: ['jobUpdates', filters],
+        queryKey: ['jobUpdates', filtersWithoutPage],
         queryFn: () => jobsApi.getJobs({ ...filters, page: 1 }),
         refetchInterval: 5 * 60 * 1000,
     });
